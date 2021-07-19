7f72ae841f23454f0ba478265104d9babee70516
Torin Bolander <bol19001@byui.edu>
2021-07-19 19:27:37 -0400

    Major update including save/load

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Overview
-This program is a very basic Character Sheet helper and die manager for a tabletop role playing game. Currently, all player information needs to be entered at runtime.
+This program is a very basic Character Sheet helper and die manager for a tabletop role playing game. This information will need entered once for each sheet and each level where values would be changed, and only one can be handled by the program at a time at present, but the values can be saved and loaded from local .json files made in the structure of the CharacterSheet class.
 
 I started this project to learn basics of Python from the input/output stream through class definitions and inheritance. I
 felt it would be very helpful to me as a dungeon master who often does not have a personal character sheet on hand to have a program that can quickly be filled to cleanly portray my characters for use as a player. Additionally, I designed it with the intent to one day expand for use as a DM for storage of enemy or NPC stat blocks.
 
-You can view a short demo of the program here: [Software Demo Video](https://youtu.be/ra9u5tOA4HA)
+You can view a short demo of the program here: [Software Demo Video](https://youtu.be/ymZnJM12EBA)
 
 # Development Environment
-* Python 3.9.4 64-bit
+* Python 3.9.5 64-bit
 * Visual Studio Code
 * Git / GitHub
 
@@ -18,6 +18,6 @@ You can view a short demo of the program here: [Software Demo Video](https://you
 * [Class Contructors](https://www.geeksforgeeks.org/constructors-in-python/)
 
 # Future Work
-* File save and load capabilities so the Character sheet information only needs input once, rather than once per program
-* Fully flesh out Character sheet information (stats, spell lists, equipment, feats and skills, etc.)
-* Design dynamic object management for enemy and NPC stat block creation, update, display, and deletion
+* Fully flesh out Character sheet information (spell lists, equipment, feats and skills, etc.)
+* Design dynamic object/file management for enemy and NPC stat block creation, update, display, and deletion
+* Implement "smart" rolls that will take in to account applicable stat modifiers for specific rolls
--- characterSheet.py
+++ characterSheet.py
@@ -1,9 +1,10 @@
 # Author: Torin Bolander
-# Program: Character Sheet and die manager
+# Program: Character Sheet and Die Manager
 
 import random
+import json
 
-# StatBlock parent class
+"""StatBlock parent class"""
 class StatBlock:
     def __init__(self):
         self.currentHealth = 0
@@ -14,18 +15,13 @@ class StatBlock:
         self.speed = 0
         self.race = ""
         self.level = 0
-    
-    # Retrieval methods for each instance value
-    def getCurrent(self): return self.currentHealth
-    def getMax(self): return self.maxHealth
-    def getInitiative(self): return self.initiative
-    def getAC(self): return self.armorClass
-    def getName(self): return self.name
-    def getSpeed(self): return self.speed
-    def getRace(self): return self.race
-    def getLevel(self): return self.level
+        self.strength = 0
+        self.dex = 0
+        self.con = 0
+        self.intel = 0
+        self.wisdom = 0
+        self.cha = 0
 
-    # Updating methods for each instance value
     # changeCurrent handles both positive and negative change 
     def changeCurrent(self, change, difference):
         if difference == 0:
@@ -39,50 +35,98 @@ class StatBlock:
             else:
                 self.currentHealth += change
 
-    def setMax(self, health): 
-        self.maxHealth = health 
-        self.currentHealth = health
-    def setInitiative(self, start): self.initiative = start
-    def setAC(self, ac): self.armorClass = ac
-    def setName(self, word): self.name = word
-    def setSpeed(self, zoom): self.speed = zoom
-    def setRace(self, kin): self.race = kin
-    def setLevel(self, lvl): self.level = lvl
+    # TODO def display() for DM's
 
-    #def display()
+    # Save as .json file for perpetuation after closing
+    def save(self):
+        stats = {"currentHealth":self.currentHealth, "maxHealth":self.maxHealth, 
+                 "initiative":self.initiative, "armorClass":self.armorClass, 
+                 "name":self.name, "speed":self.speed, "race":self.race, "level":self.level,
+                 "strength":self.strength, "dex":self.dex, "con":self.con, "intel":self.intel,
+                 "wisdom":self.wisdom, "cha":self.cha}
 
-# CharacterSheet child class    
+        file_name = input("Where do you wish to save this stat block? ")
+        file = open(file_name, "w+")
+        json.dump(stats, file)
+        file.close()
+
+    # Load a saved file and fill the stat block
+    def load(self):
+        file_name = input("Please enter the file name for the stat block: ")
+        file = open(file_name, "r")
+        stats = json.load(file)
+        file.close()
+
+        self.currentHealth = stats["currentHealth"]
+        self.maxHealth = stats["maxHealth"]
+        self.initiative = stats["initiative"]
+        self.armorClass = stats["armorClass"]
+        self.name = stats["name"]
+        self.speed = stats["speed"]
+        self.race = stats["race"]
+        self.level = stats["level"]
+        self.strength = stats["strength"]
+        self.dex = stats["dex"]
+        self.con = stats["con"]
+        self.intel = stats["intel"]
+        self.wisdom = stats["wisdom"]
+        self.cha = stats["cha"]
+
+"""CharacterSheet child class"""    
 class CharacterSheet(StatBlock):
     def __init__(self):
         StatBlock.__init__(self)
         self.playerClass = ""
+        self.profBonus = 2
     
-    # CharacterSheet specific getter and setter
-    def getClass(self): return self.playerClass
-    def setClass(self, skill): self.playerClass = skill 
-
     # Output the values for the Character Sheet in a readable form
     def display(self):
-        print(f"Name: {self.getName()}   Class: {self.getClass()}    Race: {self.getRace()}\n"
-              f"Level: {self.getLevel()}    Armor Class: {self.getAC()}    Speed: {self.getSpeed()}\n"
-              f"Current Health: {self.getCurrent()}    Max Health: {self.getMax()}    "
-              f"Initiative: {self.getInitiative()}")    
+        print(f"Name: {self.name}   Class: {self.playerClass}    Race: {self.race}\n"
+              f"Level: {self.level}    Armor Class: {self.armorClass}    Speed: {self.speed}\n"
+              f"Current Health: {self.currentHealth}    Max Health: {self.maxHealth}    "
+              f"Initiative: {self.initiative}    Proficiency Bonus: +{self.profBonus}\nStrength: "
+              f"{self.strength}\nDexterity: {self.dex}\nConstitution: {self.con}\nIntelligence: "
+              f"{self.intel}\nWisdom: {self.wisdom}\nCharisma: {self.cha}")  
 
-# List of available commands for the help action to print
-commands = ["Available commands:", "cp - Fill Character Sheet",
-            "roll - Roll dice", "display - Print out Character Sheet information",
-            "heal - Heal your character", "hurt - Apply damage to character",
-            "fight - Update initiative", "help - Print out available commands", 
-            "quit - Close Program"]
-            
-# Opening message
-print ("Initializing Character Sheet manager...\n Type 'help' to see list of commands")
+    # Save as .json file for perpetuation after closing
+    def save(self):
+        stats = {"currentHealth":self.currentHealth, "maxHealth":self.maxHealth, 
+                 "initiative":self.initiative, "armorClass":self.armorClass, 
+                 "name":self.name, "speed":self.speed, "race":self.race, "level":self.level, 
+                 "class":self.playerClass,"prof":self.profBonus, "strength":self.strength, 
+                 "dex":self.dex, "con":self.con, "intel":self.intel, "wisdom":self.wisdom, 
+                 "cha":self.cha}
 
-# For future file management the step to load a saved CharacterSheet goes here.
-# For now however we will be filling a CharacterSheet object every launch
-# TODO fill in input system once 
+        file_name = input("Where do you want to save this character sheet? ")
+        file = open(file_name, "w+")
+        json.dump(stats, file)
+        file.close()
 
-# Roll function for "rolling" die
+    # Load a saved file and fill the stat block
+    def load(self): 
+        file_name = input("Please enter the file name for the character sheet: ")
+        file = open(file_name, "r")
+        stats = json.load(file)
+        file.close()
+
+        self.currentHealth = stats["currentHealth"]
+        self.maxHealth = stats["maxHealth"]
+        self.initiative = stats["initiative"]
+        self.armorClass = stats["armorClass"]
+        self.name = stats["name"]
+        self.speed = stats["speed"]
+        self.race = stats["race"]
+        self.level = stats["level"] 
+        self.playerClass = stats["class"]
+        self.profBonus = stats["prof"]
+        self.strength = stats["strength"]
+        self.dex = stats["dex"]
+        self.con = stats["con"]
+        self.intel = stats["intel"]
+        self.wisdom = stats["wisdom"]
+        self.cha = stats["cha"]
+
+""" Roll function for "rolling" die"""
 def roll (amount, sides):
     total = 0
     originalAmt = amount
@@ -94,53 +138,93 @@ def roll (amount, sides):
     # Print the final result of total rolls
     print (f"{originalAmt}d{sides}: {total}")
 
-# Main program loop
-quit = False
-myChar = CharacterSheet()
-while not quit:
-    action = input("> ")
-    if action == "quit":
-        # Set the quit condition of the loop to exit program
-        quit = True
-        print ("Thank you for using our Character sheet and die roller!")
-    elif action == "roll":
-        # Take in user input in a standard notation (ex. 1d20) and pass to roll 
-        # function for "rolling"
-        dice = input("Input dice to roll using #d# notation: ")
-        kinds = dice.split('d')
-        amount = int(kinds[0])
-        sides = int(kinds[1])
-        roll (amount, sides)
-    elif action == "cp":
-        # Fill Character Sheet
-        myChar.setName(input("Enter character name: "))
-        myChar.setClass(input("Enter character class: "))
-        myChar.setRace(input("Enter character race: "))
-        myChar.setLevel(int(input("Enter character level: ")))
-        myChar.setAC(int(input("Enter character Armor Class: ")))
-        myChar.setSpeed(int(input("Enter character movement speed: ")))
-        myChar.setMax(int(input("Enter Max Health (Warning: Current health is set"
-                                " to max during setup):  ")))
-        myChar.setInitiative(int(input("Enter current initiative (If not applicable, set to 10): ")))
-    #elif action == "csb":
-        # TODO Fill stat block object
-        # This feature is not yet designed
-    elif action == "display":
-        # Print out Character sheet
-        myChar.display()
-    elif action == "heal":
-        # Heal Character
-        myChar.changeCurrent(int(input("Heal by how much? ")), 1)
-    elif action == "hurt":
-        # Damage Character
-        myChar.changeCurrent(int(input("How much damage? ")), 0)
-    elif action == "fight":
-        # Update initiative
-        myChar.setInitiative(int(input("Enter current initiative: ")))
-    elif action == "help":
-        # Output list of available commands
-        for x in range(len(commands)):
-            print(commands[x])
+def main():
+    # List of available commands for the help action to print
+    commands = ["Available commands:", "cp - Fill Character Sheet",
+                "roll - Roll dice", "display - Print out Character Sheet information",
+                "heal - Heal your character", "hurt - Apply damage to character",
+                "fight - Update initiative", "help - Print out available commands", 
+                "quit - Close Program"]
+    quit = False
+    changed = False
+    myChar = CharacterSheet()        
+    # Opening message
+    print ("Welcome to the Character Sheet and Die Manager!\n Type 'help' to see list of commands")
+
+    # For future file management the step to load a saved CharacterSheet goes here.
+    # For now however we will be filling a CharacterSheet object every launch
+    # TODO fill in input system once 
+    if input("Do you have a saved character sheet to load (y/n)? ") == 'y':
+        myChar.load()
     else:
-        # Error case for nonexistent commands, typos, etc.
-        print ("Input not recognized please try again")
+        print ("Please fill out a Character Sheet with the 'cp' command.")
+
+    # Main program loop
+    while not quit:
+        action = input("> ")
+        if action == "quit":
+            # Set the quit condition of the loop to exit program
+            if changed:
+                if input("Changes have been made since last saving, would you like to save before quitting (y/n)? ") == 'y':
+                    myChar.save()
+            quit = True
+            print ("Thank you for using our Character sheet and die roller!")
+        elif action == "roll":
+            # Take in user input in a standard notation (ex. 1d20) and pass to roll 
+            # function for "rolling"
+            dice = input("Input dice to roll using #d# notation: ")
+            kinds = dice.split('d')
+            amount = int(kinds[0])
+            sides = int(kinds[1])
+            roll (amount, sides)
+        elif action == "cp":
+            # Fill Character Sheet
+            myChar.name = input("Enter character name: ")
+            myChar.playerClass = input("Enter character class: ")
+            myChar.race = input("Enter character race: ")
+            myChar.level = int(input("Enter character level: "))
+            myChar.profBonus = int(input("Enter character proficiency bonus: "))
+            myChar.armorClass = int(input("Enter character Armor Class: "))
+            myChar.speed = int(input("Enter character movement speed: "))
+            myChar.maxHealth = int(input("Enter Max Health: "))
+            myChar.currentHealth = int(input("Enter Current Health: "))
+            myChar.initiative = int(input("Enter current initiative (If not applicable, set to 10): "))
+            myChar.strength = int(input("Enter character Strength: "))
+            myChar.dex = int(input("Enter character Dexterity: "))
+            myChar.con = int(input("Enter character Constitution: "))
+            myChar.intel = int(input("Enter character Intelligence: "))
+            myChar.wisdom = int(input("Enter character Wisdom: "))
+            myChar.cha = int(input("Enter character Charisma: "))
+            changed = True
+        #elif action == "csb":
+            # TODO Fill stat block object
+            # Dungeon Master features are not yet designed
+        elif action == "display":
+            # Print out Character sheet
+            myChar.display()
+        elif action == "heal":
+            # Heal Character
+            myChar.changeCurrent(int(input("Heal by how much? ")), 1)
+            changed = True
+        elif action == "hurt":
+            # Damage Character
+            myChar.changeCurrent(int(input("How much damage? ")), 0)
+            changed = True
+        elif action == "fight":
+            # Update initiative
+            myChar.initiative = int(input("Enter current initiative: "))
+            changed = True
+        elif action == "help":
+            # Output list of available commands
+            for x in range(len(commands)):
+                print(commands[x])
+        elif action == "save":
+            # Save the values of the sheet
+            myChar.save()
+            changed = False
+        else:
+            # Error case for nonexistent commands, typos, etc.
+            print ("Input not recognized please try again")
+
+if __name__ == "__main__":
+    main()
